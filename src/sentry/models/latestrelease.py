d6b6aa01029afbe9c58ea18fa33f77687a132f76
Katie Byers <lobsterkatie@gmail.com>
2019-11-27 23:57:16 -0800

    rename model, update docstring and comments

--- src/sentry/models/latestrelease.py
+++ src/sentry/models/latestrelease.py
@@ -3,9 +3,10 @@ from __future__ import absolute_import, print_function
 from sentry.db.models import BoundedBigIntegerField, Model, sane_repr
 
 
-class LatestRelease(Model):
+class LatestRepoReleaseEnvironment(Model):
     """
-    Tracks the latest release of a given repository for a given environment.
+    For each environment, tracks the latest release which is associated with
+    commits in the given repo.
     """
 
     __core__ = False
@@ -14,12 +15,14 @@ class LatestRelease(Model):
     # 0 for 'all environments'
     environment_id = BoundedBigIntegerField()
     release_id = BoundedBigIntegerField()
+    # deploy_id and commit_id are nullable but in practice always have a value
     deploy_id = BoundedBigIntegerField(null=True)
+    # commit_id is the id of the ReleaseHeadCommit associated with the given release
     commit_id = BoundedBigIntegerField(null=True)
 
     class Meta:
         app_label = "sentry"
-        db_table = "sentry_latestrelease"
+        db_table = "sentry_latestreporeleaseenvironment"
         unique_together = (("repository_id", "environment_id"),)
 
-    __repr__ = sane_repr("repository_id", "environment_id")
+    __repr__ = sane_repr("repository_id", "environment_id", "release_id")
