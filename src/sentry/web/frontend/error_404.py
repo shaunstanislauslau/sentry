51b84aecccf8f5f7082cd8ac7b5bf1baaf9aee91
Joshua Li <josh@jrl.ninja>
2019-11-27 16:21:12 -0800

    temp stuff just to push to remote so i remember

--- src/sentry/web/frontend/error_404.py
+++ src/sentry/web/frontend/error_404.py
@@ -1,13 +1,12 @@
 from __future__ import absolute_import
 
 from django.views.generic import View
-from django.template import Context, loader
+from django.template import loader
 from django.http import HttpResponseNotFound
 
 
 class Error404View(View):
     def dispatch(self, request):
-        context = {"request": request}
-
         t = loader.get_template("sentry/404.html")
-        return HttpResponseNotFound(t.render(Context(context)))
+        # so previously it didn't seem to mind Context, but i believe this is discouraged, nede to confirm for PR description
+        return HttpResponseNotFound(t.render(request=request))
